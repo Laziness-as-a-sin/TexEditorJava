fc251ca10674f8337b0a2a84a51364b7ba8feff2
Pavel <pavel20125@gmail.com>
2019-04-29 08:38:37 +1000

    add

--- TextEditor/src/Paragraph.java
+++ TextEditor/src/Paragraph.java
@@ -1,12 +1,13 @@
 import org.json.JSONObject;
 import org.json.JSONException;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Paragraph {
     
     int x, y;
-    JSONObject p = new JSONObject();
-    
+    //JSONObject p = new JSONObject();
+    ArrayList<JSONObject> t = new ArrayList();
     public Paragraph(int x1, int y1) throws JSONException{
         x = x1;
         y = y1;
@@ -16,4 +17,9 @@ public class Paragraph {
         
     }
     
+    public void add(char tec) throws JSONException{
+        JSONObject p = new JSONObject();
+        p.put("Text", tec);
+        t.add(p);
+    }
 }
